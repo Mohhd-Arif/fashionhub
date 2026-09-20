b02dc5ec82a9cdc53a867be1da62e0ff7a227a1b
mohhd-arif <arifali123454150@gmail.com>
2026-09-20 14:58:53 +0530

    dist dile update

--- FHUB_be/index.js
+++ FHUB_be/index.js
@@ -7,9 +7,7 @@ const { protectWrites } = require('./service/auth');
 const { ensureIndexes } = require('./config/indexes');
 const { retryImageCleanup } = require('./service/images');
 const server = express();
-const frontendDist = process.env.FRONTEND_DIST
-  ? path.resolve(process.env.FRONTEND_DIST)
-  : path.resolve(__dirname, '../FHUB_ui/dist');
+const frontendDist = path.resolve(__dirname, './dist');
 const frontendIndex = path.join(frontendDist, 'index.html');
 
 server.disable('x-powered-by');
